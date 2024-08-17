f73c33ca4313930a4985e60c8e4fbacddab9ea4e
atmin009 <suppanat.ku@mail.wu.ac.th>
2024-08-17 17:53:48 +0700

    add AuthProviderและแก้ไข เส้นทาง path  +ตรวจสอบการเข้าใช้งานระบบ

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // เปลี่ยน Switch เป็น Routes และ Route
-import HomePage from './pages/index';
-import OtherPage from './pages/other';
-import AdminPredictPage from './pages/admin/predict';
-import RegisterPage from './pages/register';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/index";
+import AdminPage from "./pages/admin/index";
+import EmployeePage from "./pages/employee/index";
+import CEOPage from "./pages/ceo/index";
+import OtherPage from "./pages/other/index";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./contexts/AuthContext";
+import RegisterPage from "./pages/register";
 
 function AppRouter() {
   return (
+    <AuthProvider>
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/other" element={<OtherPage />} />
-        <Route path="/admin/predict" element={<AdminPredictPage />} />
 
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedStatus={[0]}>
+              <AdminPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/employee"
+          element={
+            <ProtectedRoute allowedStatus={[1]}>
+              <EmployeePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/ceo"
+          element={
+            <ProtectedRoute allowedStatus={[2]}>
+              <CEOPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/other"
+          element={
+            <ProtectedRoute allowedStatus={[3]}>
+              <OtherPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
+    </AuthProvider>
   );
 }
 
