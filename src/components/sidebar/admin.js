075ec71e871a771d9da639cef83eedac9ccd1ccd
atmin009 <suppanat.ku@mail.wu.ac.th>
2024-08-20 00:28:54 +0700

    add icons

--- src/components/sidebar/admin.js
+++ src/components/sidebar/admin.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Nav, Offcanvas, NavDropdown } from "react-bootstrap";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaHome, FaChartLine, FaDatabase, FaUserCog } from "react-icons/fa";
 import "./admin.css";
 
 const AdminSidebar = ({ show, handleClose, handleShow }) => {
@@ -16,22 +16,46 @@ const AdminSidebar = ({ show, handleClose, handleShow }) => {
 
       {/* Sidebar สำหรับ Desktop */}
       <div className="d-none d-md-block bg-light custom-sidebar">
-  <div className="text-center mb-4">
-  </div>
-  <Nav className="flex-column" style={{ fontFamily: "Anuphan" }}>
-    <img src="/logo/logo.png" alt="Logo" style={{ width: "100%", marginBottom: "20px" }} />
-    <Nav.Link href="/admin">หน้าแรก</Nav.Link>
-    <Nav.Link href="/admin/predict">การพยากรณ์ไฟฟ้า</Nav.Link>
-    <NavDropdown title="จัดการข้อมูล" id="desktop-nav-dropdown">
-      <NavDropdown.Item href="#action/3.1">จำนวน unit ไฟฟ้า</NavDropdown.Item>
-      <NavDropdown.Item href="#action/3.2">จำนวนผู้ใช้ไฟฟ้า</NavDropdown.Item>
-      <NavDropdown.Item href="#action/3.3">อาคารและพื้นที่การใช้สอย</NavDropdown.Item>
-      <NavDropdown.Item href="#action/3.4">การสอบและเปิดปิดภาคเรียน</NavDropdown.Item>
-    </NavDropdown>
-    <Nav.Link href="#manageusers">จัดการผู้ใช้งานระบบ</Nav.Link>
-  </Nav>
-</div>
-
+        <div className="text-center mb-4">
+        </div>
+        <Nav className="flex-column" style={{ fontFamily: "Anuphan" }}>
+          <img src="/logo/logo.png" alt="Logo" style={{ width: "100%", marginBottom: "20px" }} />
+          
+          <Nav.Link href="/admin">
+            <FaHome style={{ marginRight: "8px" }} />
+            หน้าแรก
+          </Nav.Link>
+          
+          <Nav.Link href="/admin/predict">
+            <FaChartLine style={{ marginRight: "8px" }} />
+            การพยากรณ์ไฟฟ้า
+          </Nav.Link>
+          
+          <NavDropdown title={<><FaDatabase style={{ marginRight: "8px" }} /> จัดการข้อมูล</>} id="desktop-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">
+              <FaChartLine style={{ marginRight: "8px" }} />
+              จำนวน unit ไฟฟ้า
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              <FaChartLine style={{ marginRight: "8px" }} />
+              จำนวนผู้ใช้ไฟฟ้า
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">
+              <FaDatabase style={{ marginRight: "8px" }} />
+              อาคารและพื้นที่การใช้สอย
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">
+              <FaDatabase style={{ marginRight: "8px" }} />
+              การสอบและเปิดปิดภาคเรียน
+            </NavDropdown.Item>
+          </NavDropdown>
+          
+          <Nav.Link href="#manageusers">
+            <FaUserCog style={{ marginRight: "8px" }} />
+            จัดการผู้ใช้งานระบบ
+          </Nav.Link>
+        </Nav>
+      </div>
 
       {/* Offcanvas สำหรับโหมดมือถือ */}
       <Offcanvas show={show} onHide={handleClose} placement="start">
@@ -40,15 +64,36 @@ const AdminSidebar = ({ show, handleClose, handleShow }) => {
         </Offcanvas.Header>
         <Offcanvas.Body>
           <Nav className="flex-column">
-            <Nav.Link href="/admin">หน้าแรก</Nav.Link>
-            <Nav.Link href="/admin/predict">การพยากรณ์ไฟฟ้า</Nav.Link>
-            <NavDropdown title="จัดการข้อมูล" id="mobile-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">จำนวน unit ไฟฟ้า</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">จำนวนผู้ใช้ไฟฟ้า</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">อาคารและพื้นที่การใช้สอย</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">การสอบและเปิดปิดภาคเรียน</NavDropdown.Item>
+            <Nav.Link href="/admin">
+              <FaHome style={{ marginRight: "8px" }} />
+              หน้าแรก
+            </Nav.Link>
+            <Nav.Link href="/admin/predict">
+              <FaChartLine style={{ marginRight: "8px" }} />
+              การพยากรณ์ไฟฟ้า
+            </Nav.Link>
+            <NavDropdown title={<><FaDatabase style={{ marginRight: "8px" }} /> จัดการข้อมูล</>} id="mobile-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                <FaChartLine style={{ marginRight: "8px" }} />
+                จำนวน unit ไฟฟ้า
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                <FaChartLine style={{ marginRight: "8px" }} />
+                จำนวนผู้ใช้ไฟฟ้า
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                <FaDatabase style={{ marginRight: "8px" }} />
+                อาคารและพื้นที่การใช้สอย
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                <FaDatabase style={{ marginRight: "8px" }} />
+                การสอบและเปิดปิดภาคเรียน
+              </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#manageusers">จัดการผู้ใช้งานระบบ</Nav.Link>
+            <Nav.Link href="#manageusers">
+              <FaUserCog style={{ marginRight: "8px" }} />
+              จัดการผู้ใช้งานระบบ
+            </Nav.Link>
           </Nav>
         </Offcanvas.Body>
       </Offcanvas>
